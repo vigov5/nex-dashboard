a7ad95ab5c73c76c60e4fa7fe7ac3d54ba7d99b4
vigov5 <nguyenanhtien2210@gmail.com>
2023-12-26 15:47:41 +0700

    End chapter 12

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	experimental: {
+		serverActions: {
+			allowedForwardedHosts: ['localhost', '0.0.0.0'],
+			allowedOrigins: ['localhost:3000', '0.0.0.0:3000']
+		},
+	}
+};
 
 module.exports = nextConfig;
